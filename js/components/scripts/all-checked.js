8e5275645aa316c96205e379c024476bb65855c8
Alex <format255@bk.ru>
2017-09-03 18:49:47 +0300

    19

---
+++ js/components/scripts/all-checked.js
@@ -0,0 +1,7 @@
+$('.all_js').on('click', function() {
+   $(this).closest('.page-content').find('input:checkbox').prop('checked', true);
+});
+
+$('.none_js').on('click', function() {
+   $(this).closest('.page-content').find('input:checkbox').prop('checked', false);
+});

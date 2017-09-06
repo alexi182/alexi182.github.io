3570758c8f09b26cb22ce5898cdeccdcabda81de
alex182 <format255@bk.ru>
2017-09-06 16:45:56 +0300

    51

--- js/components/scripts/right-window-scroll.js
+++ js/components/scripts/right-window-scroll.js
@@ -2,8 +2,10 @@ $('.profile-link').on('click', function () {
    var root = $(this).parent();
    if(root.hasClass('layout-links__item_closed-profile')){
       root.removeClass('layout-links__item_closed-profile').addClass('layout-links__item_opened');
+      $('body').addClass('opened');
    } else {
       root.removeClass('layout-links__item_opened').addClass('layout-links__item_closed-profile');
+      $('body').removeClass('opened');
    }
 });
 
@@ -15,6 +17,15 @@ $('.search-link').on('click', function () {
       root.removeClass('layout-links__item_opened').addClass('layout-links__item_closed-search');
    }
 });
+
+/*
+
+var sidebar_profile = $('.layout-links__item');
+   $(document,'.opened').on('click', function () {
+      sidebar_profile.removeClass('layout-links__item_opened').addClass('layout-links__item_closed-profile');
+
+});
+*/
 
 
 

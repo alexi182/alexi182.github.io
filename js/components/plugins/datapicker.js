8c8e12196d2e622d961fa7c63fc3b6aa604d1aa8
alex182 <format255@bk.ru>
2017-09-08 16:45:19 +0300

    58

--- js/components/plugins/datapicker.js
+++ js/components/plugins/datapicker.js
@@ -1,6 +1,22 @@
 require('../../vendor/jquery-ui');
 
-$(".datepicker").datepicker();
+var settings = {
+   changeYear: true,
+   changeMonth: true,
+   dateFormat: "dd.mm.yy",
+   yearRange: "-90:+90",
+   dayNamesMin: ['Пн', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Вс'],
+   monthNamesShort: ["Янв", "Фев", "Мар", "Апр", "Май", "Июн", "Июл", "Авг", "Сен", "Окт", "Ноя", "Дек"],
+   monthNames: ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"]
+};
+
+$(".datepicker").datepicker(
+    settings
+);
+
+$('.dateblock').datepicker(
+    settings
+);
 
 
 

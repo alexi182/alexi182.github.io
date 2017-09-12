a4ab03f5db55dc3589904225c4df133a1ee9c54d
Alex <format255@bk.ru>
2017-09-12 23:05:31 +0300

    77

--- js/components/scripts/comment-open.js
+++ js/components/scripts/comment-open.js
@@ -1,10 +1,10 @@
-$('.page-content__post-link').on('click', function(e) {
+$('.open_js').on('click', function(e) {
    e.preventDefault();
    var comment = $(this).closest('.page-content__post').find('.page-content__comment');
    comment.removeClass('comment-hide').addClass('comment-show');
 });
 
-$('.icon-closed').on('click', function(e) {
+$('.close_js').on('click', function(e) {
    e.preventDefault();
    var comment = $(this).parent('.page-content__comment');
    comment.removeClass('comment-show').addClass('comment-hide');

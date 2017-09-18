364e965504c0eeb550c2a58cf5903635f3214a23
Alex <format255@bk.ru>
2017-09-18 23:21:28 +0300

    modals

--- js/components/scripts/modal-window.js
+++ js/components/scripts/modal-window.js
@@ -1,7 +1,14 @@
-$('.overlay').fadeIn(500);
+
+$('.file-list').on('click', function() {
+   $(this).addClass('window1-active');
+   $(this).parent().siblings('.page-content__inner').find('.window-1').show();
+   $('.overlay').show();
+});
 
 $('.button-close_js').on('click', function() {
-	$(this).parent('.window').fadeOut(500);
+   $(this).parent().parent().siblings('.page-content-row').find('.file-list').removeClass('window1-active');
+	$(this).parent('.window').hide();
+	$('.overlay').hide();
 });
 
 

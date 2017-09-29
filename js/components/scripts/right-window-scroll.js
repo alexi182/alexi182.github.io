c5c0bd4171475f8f600c1d000e9dc19da4f82311
alex182 <format255@bk.ru>
2017-09-29 15:24:28 +0300

    work2

--- js/components/scripts/right-window-scroll.js
+++ js/components/scripts/right-window-scroll.js
@@ -1,32 +1,40 @@
 $('.profile-link').on('click', function () {
    var root = $(this).parent();
+   var search = $(this).parent().parent().siblings('.layout-links_search').find('.sidebar-search');
    if(root.hasClass('layout-links__item_closed-profile')){
       root.removeClass('layout-links__item_closed-profile').addClass('layout-links__item_opened');
       $('body').addClass('opened');
+      $('.overlay').show();
    } else {
       root.removeClass('layout-links__item_opened').addClass('layout-links__item_closed-profile');
       $('body').removeClass('opened');
+      if (!search.hasClass('layout-links__item_opened')) {
+         $('.overlay').hide();
+      }
    }
 });
 
 $('.search-link').on('click', function () {
    var root = $(this).parent();
+   var profile = $(this).parent().parent().siblings('.layout-links_profile').find('.sidebar-profile');
    if(root.hasClass('layout-links__item_closed-search')){
       root.removeClass('layout-links__item_closed-search').addClass('layout-links__item_opened');
+      $('.overlay').show();
+     /* profile.css('background', 'red');*/
    } else {
       root.removeClass('layout-links__item_opened').addClass('layout-links__item_closed-search');
+      if (!profile.hasClass('layout-links__item_opened')) {
+         $('.overlay').hide();
+      }
    }
 });
 
-/*
+$('.overlay').on('click', function() {
+   $(this).siblings('.stick-footer').find('.sidebar-profile').removeClass('layout-links__item_opened').addClass('layout-links__item_closed-profile');
 
-var sidebar_profile = $('.layout-links__item');
-   $(document,'.opened').on('click', function () {
-      sidebar_profile.removeClass('layout-links__item_opened').addClass('layout-links__item_closed-profile');
-
+   $(this).siblings('.stick-footer').find('.sidebar-search').removeClass('layout-links__item_opened').addClass('layout-links__item_closed-search');
+   $(this).hide();
 });
-*/
-
 
 
 

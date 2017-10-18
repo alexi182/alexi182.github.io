707ea5e0f6ac9f72e3fe3c201cb520482dff5840
alex182 <format255@bk.ru>
2017-10-18 12:40:42 +0300

    subjects

---
+++ js/components/scripts/file-upload-close.js
@@ -0,0 +1,12 @@
+$(".file-upload input[type=file]").on('change', function () {
+   var filename = $(this).val().replace(/.*\\/, "");
+   $('.files').append(
+      '<div class="file-block"><div class="file">' + filename + '<a href="#" class="file-close js-file-close"></a></div>'
+   );
+});
+
+ $('.files').on('click', '.js-file-close', function (e) {
+    e.preventDefault();
+    $(this).parent().parent('.file-block').remove();
+ });
+

339a6d198e37e76570e5950332c79c33c89688cb
Alex <format255@bk.ru>
2017-10-22 22:36:01 +0300

    new

--- js/components/scripts/all-checked.js
+++ js/components/scripts/all-checked.js
@@ -1,8 +1,10 @@
-$('.all_js').on('click', function() {
-   $(this).closest('.page-content').find('input:checkbox').prop('checked', true);
-});
-
-$('.none_js').on('click', function() {
-   $(this).closest('.page-content').find('input:checkbox').prop('checked', false);
+$('.all_js').on('click', function () {
+   if (!$(this).hasClass('checked')) {
+      $(this).addClass('checked');
+      $(this).closest('.page-content').find('input:checkbox').prop('checked', true);
+   } else {
+      $(this).removeClass('checked');
+      $(this).closest('.page-content').find('input:checkbox').prop('checked', false);
+   }
 });
 
